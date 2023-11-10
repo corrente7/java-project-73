7f863c1ad15824464413f1565546698b6a5b454f
Nadia Makeeva <lossa@inbox.ru>
2023-11-10 23:57:43 +0300

    add 1 file

---
+++ src/main/java/hexlet/code/config/RollbarConfig.java
@@ -0,0 +1,33 @@
+package hexlet.code.config;
+
+
+import com.rollbar.notifier.Rollbar;
+import com.rollbar.notifier.config.Config;
+import com.rollbar.spring.webmvc.RollbarSpringConfigBuilder;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration()
+@EnableWebMvc
+@ComponentScan({
+        // ADD YOUR PROJECT PACKAGE HERE
+        "hexlet.code"
+})
+
+public class RollbarConfig {
+
+    public Rollbar rollbar() {
+        return new Rollbar(getRollbarConfigs("<ACCESS_TOKEN>"));
+    }
+
+    private Config getRollbarConfigs(String accessToken) {
+
+        // Reference ConfigBuilder.java for all the properties you can set for Rollbar
+        return RollbarSpringConfigBuilder.withAccessToken(accessToken)
+                .environment("development")
+                .codeVersion("1.0.0")
+                .build();
+    }
+}
+}
